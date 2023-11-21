1f3aa93dd395898290df5c01306745054c7d5884
Ajinkya Patil <ajinkyap331@gmail.com>
2023-11-21 22:00:48 +0530

    added 7 in cpp

--- CPP/7_.cpp
+++ CPP/7_.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
 
 int gcd(int a, int b)
 {
     if (a < b)
-    {
         return gcd(b, a);
-    }
-    else if (a % b == 0)
-    {
+    if (a % b == 0)
         return b;
-    }
-    else
+    return gcd(b, a % b);
+}
+
+int gen_key(int q)
+{
+    int key;
+    do
     {
-        return gcd(b, a % b);
-    }
+        cout << "Enter a private key (should be a large random number): ";
+        cin >> key;
+    } while (gcd(q, key) != 1);
+    return key;
 }
 
 int power(int a, int b, int c)
 {
     int x = 1;
     int y = a;
-
     while (b > 0)
     {
         if (b % 2 != 0)
@@ -36,82 +35,65 @@ int power(int a, int b, int c)
         y = (y * y) % c;
         b = b / 2;
     }
-
     return x % c;
 }
 
-int gen_key(int q)
+pair<vector<int>, int> encrypt(const string &msg, int q, int h, int g)
 {
-    long long int key = rand() % (int)pow(10, 10) + q;
-    while (gcd(q, key) != 1)
-    {
-        key = rand() % (int)pow(10, 10) + q;
-    }
-    return key;
-}
-
-vector<int> encrypt(const string &msg, int q, int h, int g)
-{
-    vector<int> en_msg(msg.length());
-
+    vector<int> en_msg;
     int k = gen_key(q);
     int s = power(h, k, q);
     int p = power(g, k, q);
-
-    cout << "g^k used : " << p << endl;
-    cout << "g^ak used : " << s << endl;
-
-    for (size_t i = 0; i < msg.length(); ++i)
+    for (char ch : msg)
     {
-        en_msg[i] = s * int(msg[i]);
+        en_msg.push_back(s * int(ch));
     }
-
-    cout << "en_msg : ";
-    for (int val : en_msg)
-    {
-        cout << val << " ";
-    }
-    cout << endl;
-
-    return en_msg;
+    cout << "g^k used: " << p << endl;
+    cout << "g^ak used: " << s << endl;
+    return make_pair(en_msg, p);
 }
 
 string decrypt(const vector<int> &en_msg, int p, int key, int q)
 {
     string dr_msg;
-
     int h = power(p, key, q);
     for (int val : en_msg)
     {
-        dr_msg.push_back(char(val / h));
+        dr_msg += char(val / h);
     }
-
     return dr_msg;
 }
 
 int main()
 {
-    srand(time(0));
+    string msg;
+    cout << "Enter the message to be encrypted: ";
+    cin >> msg;
 
-    string msg = "Ajinkya";
-    cout << "Original Message: " << msg << endl;
-
-    long long int q = rand() % (int)pow(10, 20) + pow(10, 10);
-    long long int g = rand() % (q - 2) + 2;
-
-    cout << "q used : " << q << endl;
-    cout << "g used : " << g << endl;
+    int q, g;
+    cout << "Enter a large prime number q: ";
+    cin >> q;
+    cout << "Enter a primitive root g: ";
+    cin >> g;
 
     int key = gen_key(q);
-    cout << "Private Key : " << key << endl;
-
     int h = power(g, key, q);
-    cout << "g^a used : " << h << endl;
 
-    vector<int> en_msg = encrypt(msg, q, h, g);
-    string dr_msg = decrypt(en_msg, h, key, q);
+    cout << "g used: " << g << endl;
+    cout << "g^a used: " << h << endl;
 
-    cout << "Decrypted Message : " << dr_msg << endl;
+    pair<vector<int>, int> encryption_result = encrypt(msg, q, h, g);
+    vector<int> en_msg = encryption_result.first;
+    int p = encryption_result.second;
+
+    string dr_msg = decrypt(en_msg, p, key, q);
+    cout << "Encrypted Message: ";
+    for (int val : en_msg)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    cout << "Decrypted Message: " << dr_msg << endl;
 
     return 0;
 }
